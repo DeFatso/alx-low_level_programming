173245161b72fb2ddced90473595f8e2a167368a
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-03-16 06:49:33 -0700

    main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 int main(void)
 {
-	int intType;
-	double doubleType;
-	char charType;
-
-	printf("Size of int: %lu byte(s)\n", sizeof(intType));
-	printf("Size of double: %lu byte(s)\n", sizeof(doubleType));
-	printf("Size of char: %lu byte(s)\n", sizeof(charType));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
