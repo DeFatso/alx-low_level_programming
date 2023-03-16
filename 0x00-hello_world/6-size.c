54dc2cf8daced644427d7e5b7cbb606f11ea5a57
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-03-16 06:23:45 -0700

    six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include<stdio.h>
+int main(void)
+{
+	int intType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of double: %lu byte(s)\n", sizeof(doubleType));
+	printf("Size of char: %lu byte(s)\n", sizeof(charType));
+	return (0);
+}
