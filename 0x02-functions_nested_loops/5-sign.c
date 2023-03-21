d3687ceeaebd6ac8a923056a346b88e1215ed216
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-03-21 06:55:48 -0700

    five

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
  * print_sign - function that prints sign.
  *
  *@n: parameter
-
+ *
  * Return: 1, 0 and -1.
  */
 int print_sign(int n)
@@ -11,22 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 }
