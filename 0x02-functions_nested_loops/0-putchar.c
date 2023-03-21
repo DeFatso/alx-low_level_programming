e7987190ba6376dd7c633f3eda4914e2a88950cc
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-03-21 00:23:15 -0700

    zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	a = '_';
+	putchar(a);
+	a = 'p';
+	putchar(a);
+	a = 'u';
+	putchar(a);
+	a = 't';
+	putchar(a);
+	a = 'c';
+	putchar(a);
+	a = 'h';
+	putchar(a);
+	a = 'a';
+	putchar(a);
+	a = 'r';
+	putchar(a);
+	a = '\n';
+	putchar(a);
+	return (0);
+}
