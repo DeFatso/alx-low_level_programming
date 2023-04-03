7739599b597d5bc2475f186f756b713d10f89fff
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-03 11:19:23 -0700

    8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * print_diagsums - prints the sum of the two diagonals 
- * @a: pointer 
+ * print_diagsums - prints the sum of the two diagonals
+ * @a: pointer
  * @size: which diagonal to add
  */
 
