b2505118ddd860183e7a7bf38ce75c3639f7253b
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-03 05:55:06 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * memset - Fills the first n bytes of the memory area
+ * _memset - Fills the first n bytes of the memory area
  *          pointed to by @s with the constant byte @c.
  * @s: A pointer to the memory area to be filled.
  * @b: The character to fill the memory area with.
@@ -12,7 +12,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
-	
+
 	for (a = 0; a < n; a++)
 	{
 		s[a] = b;
