30f82c458596e424560f4407f8c5de10465c30b1
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-07 11:13:20 -0700

    libmy.a

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ *_atoi - entry point
+ *@s : parameter
+ *Return : returns 
+ */
+int _atoi(char *s)
+{
+	(return atoi(s));
+}
--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,10 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ *
+ * @s : input
+ * @accept : input
+ *
+ * Return: returns null
+ */
 char *_strpbrk(char *s, char *accept)
 {
-	return strpbrk(s, accept);
+	int x, n;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[x] == accept[n])
+				return (s + x);
+		}
+	}
+	return (NULL);
 }
