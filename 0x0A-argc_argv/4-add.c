a30ae5eea9577810d98d953b8d5ea9fbe6cc3125
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-09 11:38:10 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,32 +11,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, mul = 0;
+int a, mul = 0;
 
-	if (argc > 1)
-	{
-	for (a = 1; a < argc; a++)
-	{
-	char *arg = argv[a];
-	int i;
-
-	for (i = 0; arg[i] != '\0'; i++)
-	{
-	if (!isdigit(arg[i]))
-	{
-	printf("Error\n");
-	return (1);
-	}
-	}
-	mul += atoi(arg);
-	}
-	printf("%d\n", mul);
-	}
-	else
-	{
-	printf("Error\n");
-	return (1);
-	}
-	return (0);
+if (argc > 1)
+{
+for (a = 1; a < argc; a++)
+{
+char *arg = argv[a];
+int i;
+for (i = 0; arg[i] != '\0'; i++)
+{
+if (!isdigit(arg[i]))
+{
+printf("Error\n");
+return (1);
 }
-
+}
+mul += atoi(arg);
+}
+printf("%d\n", mul);
+}
+else
+{
+mul = 0;
+printf("%d\n", mul);
+}
+return (0);
+}
