87e65711664e7d51bb8188c1a6da9edc3330124c
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-09 06:59:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *
+ *@argv : parameter
+ *@argc : parrameter one
+ * Return: returns argc
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
