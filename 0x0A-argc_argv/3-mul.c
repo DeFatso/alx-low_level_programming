8a76ec5e9bc7198e64c4131e1c1e65efcc6cac06
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-09 09:55:08 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - main function
+ * @argc : parameter 1
+ * @argv : parameter 2
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, mul = 1;
+
+	if (argc > 1)
+	{
+	for (a = 1; a < argc; a++)
+	mul *= atoi(argv[a]);
+	printf("%d\n", mul);
+	}
+	else
+	{
+	printf("error\n");
+	return (1);
+	}
+
+	return (0);
+}
+
