8844609171defd88b1631df7adb6240582b2e11f
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-11 10:00:43 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 {
 	size_t len1 = strlen(s1);
 	size_t len2 = strlen(s2);
-	char *result = malloc((len1 + len2 + 1) * sizeof(char));
+	char *result = malloc(len1 + len2 + 1);
 
 	if (s1 == NULL)
 	{
