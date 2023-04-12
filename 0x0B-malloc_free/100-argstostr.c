0a793354052ea21afa2a71a6193ca5c783818fd0
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-12 09:32:19 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,13 +13,13 @@ char *argstostr(int ac, char **av)
 int i, j, len = 0;
 char *str;
 str = malloc(sizeof(char) * len);
-str[len - 1] = '\0';
+/*str[len - 1] = '\0';*/
 
 for (i = 0; i < ac; i++)
 {
 	len += strlen(av[i]) + 1;
 }
-malloc(sizeof(char) * len);
+malloc(sizeof(char) * len + 1);
 
 if (ac == 0 || av == 0)
 {
