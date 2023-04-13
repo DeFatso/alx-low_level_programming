0ca0a6e0bc56bbb81174179bca07e949a9864aba
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-13 11:11:54 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,25 +12,26 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-size_t len1 = strlen(s1);
-size_t len2 = strlen(s2);
-size_t len = len1 + ((len2 < n) ? len2 : n);
-char *result = malloc(len + 1);
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+	size_t len = len1 + ((len2 < n) ? len2 : n);
+	char *result = malloc(len + 1);
 
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
-if (result == NULL)
-{
-exit(EXIT_FAILURE);
-}
-strncpy(result, s1, len1);
-strncat(result, s2, len - len1);
-result[len] = '\0';
-return (result);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (result == NULL)
+	{
+		exit(EXIT_FAILURE);
+		return (NULL);
+	}
+	strncpy(result, s1, len1);
+	strncat(result, s2, len - len1);
+	result[len] = '\0';
+	return (result);
 }
