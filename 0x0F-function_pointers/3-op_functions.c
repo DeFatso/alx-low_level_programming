404f1c0b42ecb53afa2cb8662a979e2d8cb2bf5a
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-19 08:44:47 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ *op_add - adds two integers
+ *@a: parameter 1
+ *@b: parameter 2
+ * Return: returns an integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - subs two integers
+ *@a: parameter 1
+ *@b: parameter 2
+ * Return: returns an integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - mul two integers
+ *@a: parameter 1
+ *@b: parameter 2
+ * Return: returns an integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - divs two integers
+ *@a: parameter 1
+ *@b: parameter 2
+ * Return: returns an integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - mods two integers
+ *@a: parameter 1
+ *@b: parameter 2
+ * Return: returns an integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
