ad9614c0f31832902e2ceb2e783d15e76d0655a6
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-28 00:00:19 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,25 +1,29 @@
 #include "main.h"
 #include <math.h>
 /**
- * is_prime_number - function that determain is number is prime
+ * is_prime - function that determain is number is prime
  * Return: returns int
  * @n :parameter
+ * @i :parameter
+ */
+int is_prime(int n, int i)
+{
+	if (n < 2)
+		return (0);
+	if (i > sqrt(n))
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (is_prime(n, i + 1));
+}
+/**
+ * is_prime_number - Checks if a number is a prime number
  *
+ * @n: The number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-		return (0);
-	else if (n == 2)
-		return (1);
-
-	for (i = 2; i <= sqrt(n); i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-
-	return (1);
+	return (is_prime(n, 2));
 }
