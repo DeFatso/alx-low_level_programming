b3ec08d73641083aa4fccb5ca51d2e8a358f46b6
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-27 23:18:48 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,19 +6,26 @@
  */
 int main(void)
 {
-	int a, b;
+	int a, b, c;
 
 	for (a = 48; a <= 57; a++)
+	{
 		for (b = a + 1; b <= 57; b++)
 		{
-			putchar(a);
-			putchar(b);
-			if (a != 57 && b != 56)
+			for (c = a + 1; c <= 57; c++)
 			{
-			putchar(',');
+				putchar(a);
+				putchar(b);
+				putchar(c);
+
+				if (a != 55 || b != 57 || c != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			putchar(' ');
 		}
+	}
 	putchar('\n');
 
 	return (0);
