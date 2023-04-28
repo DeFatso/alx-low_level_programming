a00d2b0117e2bc81ab4d2ff2098ba2a2e8082aa8
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-27 23:49:46 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,19 +8,17 @@
  */
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
-	{
-	return (0);
-	}
+		return (0);
+	else if (n == 2)
+		return (1);
 
-	int sqrt_n = (int) sqrt(n);
-
-	for (int i = 2; i <= sqrt_n; i++)
+	for (i = 2; i <= sqrt(n); i++)
 	{
-	if (n % i == 0)
-	{
-	return (0);
-	}
+		if (n % i == 0)
+			return (0);
 	}
 
 	return (1);
