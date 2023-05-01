f2ead2c2164ba13cf2ff96274b0ed3d95edc91ae
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-04-30 22:17:07 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,44 +9,41 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *previous_node, *current_node;
+	listint_t *new_node, *prev_node, *curr_node;
 	unsigned int i;
 
-	if (head == NULL || idx == 0)
-	{
-		return (add_nodeint(head, n));
-	}
-
-	previous_node = NULL;
-	current_node = *head;
-
-	for (i = 0; i < idx && current_node != NULL; i++)
-	{
-		previous_node = current_node;
-		current_node = current_node->next;
-	}
-
-	if (i < idx)
-	{
+	if (head == NULL)
 		return (NULL);
+	if (idx == 0)
+	{
+		new_node = malloc(sizeof(listint_t));
+
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
+	prev_node = NULL;
+	curr_node = *head;
+
+	for (i = 0; i < idx && curr_node != NULL; i++)
+	{
+		prev_node = curr_node;
+		curr_node = curr_node->next;
+	}
+
+	if (i != idx)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 	new_node->n = n;
-	new_node->next = current_node;
-
-	if (previous_node != NULL)
-	{
-		previous_node->next = new_node;
-	}
-	else
-	{
-		*head = new_node;
-	}
+	new_node->next = curr_node;
+	prev_node->next = new_node;
 
 	return (new_node);
 }
+
