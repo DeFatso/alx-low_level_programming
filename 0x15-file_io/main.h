12ac0000efc7781f1003e2c5d796e067c25b998e
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-05-08 11:54:07 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,12 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+void copy_file(const char *file_from, const char *file_to);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
