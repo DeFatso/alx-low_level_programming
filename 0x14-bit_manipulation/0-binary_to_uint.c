7394edfa5e64fe7e6e0da6f3cb217d45267d0c1a
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-05-08 21:35:23 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,15 +6,27 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result;
+	unsigned int result = 0;
+	int i;
 
-	size_t i, length = strlen(b);
+	if (b == NULL)
+	{
+		return (0);
+	}
 
-	for (i = 0; i < length; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '1')
 		{
-			result += 1 << (length -i - 1);
+			result = (result << 1) | 1;
+		}
+		else if (b[i] == '0')
+		{
+			result = result << 1;
+		}
+		else
+		{
+			return (0);
 		}
 	}
 	return (result);
