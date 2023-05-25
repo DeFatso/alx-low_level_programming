0698951768e61f66a0b73493e8135b7614dd8fa4
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-05-25 13:36:33 +0000

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <fcntl.h>
+#include <elf.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,6 +12,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
+
+void elf_header(const Elf64_Ehdr *header);
 void copy_file(const char *file_from, const char *file_to);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
