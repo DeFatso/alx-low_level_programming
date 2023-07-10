a5bf86cc5e9e19a4d06ab9294919de6688cc66f2
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-07-10 11:55:38 +0000

    2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - creates a file.
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to the file
- * Return: Returns: 1 on success, -1 on failure 
+ * Return: Returns: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
