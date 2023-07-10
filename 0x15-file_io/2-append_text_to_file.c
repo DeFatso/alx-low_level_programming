6ba7b73f652dc784969a72af0f0928de19900824
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-07-10 12:46:41 +0000

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,7 +4,7 @@
  * @filename: name of the file
  * @text_content: NULL terminated string to add at the end of the file
  */
- int append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	size_t bytes_written;
 	size_t length;
