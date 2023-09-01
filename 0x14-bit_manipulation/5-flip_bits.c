4a8c04aca376d95db2908dce69eab1526ec710bf
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-09-01 02:37:29 +0000

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,11 +10,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor_result = n ^ m;
 	unsigned int num_bits = 0;
-	int i;
+	size_t i;
 	size_t p;
 
 	p = sizeof(unsigned long int) * 8;
-	for (i = 0; i < p; i++;)
+	for (i = 0; i < p; i++)
 	{
 		num_bits += (xor_result >> i) & 1;
 	}
