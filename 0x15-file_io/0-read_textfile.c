21cc632b38a241a8fd085ed9d38c2eeec28d4629
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-09-04 15:31:15 +0000

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: filename
+ * @letters: number of letters it should read and print
+ * Return: number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	char *buffer;
+	size_t bytes_read;
+	size_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return 0;
+	}
+
+	file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		return 0;
+	}
+
+	buffer = (char *)malloc(letters + 1);
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return 0;
+	}
+
+	bytes_read = fread(buffer, sizeof(char), letters, file);
+	if (bytes_read <= 0)
+	{
+		fclose(file);
+		free(buffer);
+		return 0;
+	}
+
+	buffer[bytes_read] = '\0';
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+
+	fclose(file);
+	free(buffer);
+
+	return (bytes_read);
+}
