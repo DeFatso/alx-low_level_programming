7d788af391b1f1345dae4dd395f1bf61c9413158
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-09-04 17:25:57 +0000

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,5 +14,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int open_file(const char *file_name, int flags, mode_t mode);
+void copy_file(int fd_from, int fd_to);
+void close_file(int fd);
+int main(int argc, char *argv[]);
 
 #endif
